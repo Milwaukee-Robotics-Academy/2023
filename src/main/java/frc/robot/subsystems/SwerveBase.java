492253a918268db01ecfebb444dc692e63c85636
Erik <ErikSLee@gmail.com>
2023-02-24 05:52:00 -0600

    Features/neo turnto
    * combining setheading with turn to angle
    
    * limit to 180-180
    
    * continuous input for drift correction
    
    * Turn tuning
    
    * Add Operator control

--- src/main/java/frc/robot/subsystems/SwerveBase.java
+++ src/main/java/frc/robot/subsystems/SwerveBase.java
@@ -1,13 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.Swerve;
-
-import java.lang.reflect.Method;
-
 import com.kauailabs.navx.frc.AHRS;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
@@ -26,6 +18,11 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Swerve;
 
 public class SwerveBase extends SubsystemBase {
 
@@ -132,6 +129,7 @@ public class SwerveBase extends SubsystemBase {
     rearLeft.getRotationMotor().setInverted(true);
     frontRight.getRotationMotor().setInverted(true);
     frontLeft.getRotationMotor().setInverted(true);
+    driftCorrectionPID.enableContinuousInput(-180, 180);
 
   }
 
