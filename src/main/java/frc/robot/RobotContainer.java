ee58f809ea933551366ed10752985565c9d18937
MRA-Laptop10 <111691692+MRA-Laptop10@users.noreply.github.com>
2023-02-27 16:24:28 -0600

    Features/falcon/turn to (#10)
    * transfer from neobase
    
    * transfer from neobase
    
    ---------
    
    Co-authored-by: Erik Lee <Erik.Lee@harley-davidson.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,25 +3,28 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.autos.*;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
     /* Controllers */
     private final Joystick driver = new Joystick(0);
-    private final Joystick operator =  new Joystick(1);
-  
+    private final Joystick operator = new Joystick(1);
 
     /* Drive Controls */
     private final int translationAxis = XboxController.Axis.kLeftY.value;
@@ -29,21 +32,28 @@ public class RobotContainer {
     private final int rotationAxis = XboxController.Axis.kRightX.value;
     private final int intakeAxis = XboxController.Axis.kLeftTrigger.value;
     private final int outtakeAxis = XboxController.Axis.kRightTrigger.value;
-
-
+    
     /* Driver Buttons */
     private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
     private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
 
-
+    public final POVButton driverUP;
+    public final POVButton driverDOWN;
+    public final POVButton driverLEFT;
+    public final POVButton driverRIGHT;
+    private POVButton operatorUP;
+    private POVButton operatorDOWN;
+    private POVButton operatorLEFT;
+    private POVButton operatorRIGHT;
 
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
 
     private final IntakeSubsystem intake = new IntakeSubsystem();
 
-
-    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
     public RobotContainer() {
         s_Swerve.setDefaultCommand(
             new TeleopSwerve(
@@ -56,9 +66,16 @@ public class RobotContainer {
             )
         );
         intake.setDefaultCommand(
-            new Intakecommand(intake, () -> driver.getRawAxis(intakeAxis), () -> driver.getRawAxis(outtakeAxis))
-        );
-
+                new Intakecommand(intake, () -> driver.getRawAxis(intakeAxis), () -> driver.getRawAxis(outtakeAxis)));
+        slow = new JoystickButton(driver, XboxController.Button.kRightBumper.value);
+        driverUP = new POVButton(driver, 0);
+        driverRIGHT = new POVButton(driver, 90);
+        driverDOWN = new POVButton(driver, 180);
+        driverLEFT = new POVButton(driver, 270);
+        operatorUP = new POVButton(operator, 0);
+        operatorRIGHT = new POVButton(operator, 90);
+        operatorDOWN = new POVButton(operator, 180);
+        operatorLEFT = new POVButton(operator, 270);
         // Configure the button bindings
         configureButtonBindings();
     }
@@ -71,14 +88,80 @@ public class RobotContainer {
         }
       }
     /**
-     * Use this method to define your button->command mappings. Buttons can be created by
+     * Return how much the speed should be reduced.
+     * 
+     * @return speed reduced percent
+     */
+    public double speedReduction() {
+        if (slow.getAsBoolean()) {
+            SmartDashboard.putBoolean("SpeedReduced", true);
+            return 0.4;
+        } else {
+            SmartDashboard.putBoolean("SpeedReduced", false);
+            return 1.0;
+        }
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
      * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+     * it to a {@link
      * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
     private void configureButtonBindings() {
         /* Driver Buttons */
         zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
+
+        driverUP.onTrue(new TurnToAngleCmd(
+                s_Swerve,
+                0.0,
+                () -> driver.getRawAxis(translationAxis),
+                () -> driver.getRawAxis(strafeAxis),
+                () -> speedReduction()));
+        driverDOWN.onTrue(new TurnToAngleCmd(
+                s_Swerve,
+                180.0,
+                () -> driver.getRawAxis(translationAxis),
+                () -> driver.getRawAxis(strafeAxis),
+                () -> speedReduction()));
+        driverRIGHT.onTrue(new TurnToAngleCmd(
+                s_Swerve,
+                -90.0,
+                () -> driver.getRawAxis(translationAxis),
+                () -> driver.getRawAxis(strafeAxis),
+                () -> speedReduction()));
+        driverLEFT.onTrue(new TurnToAngleCmd(
+                s_Swerve,
+                90.0,
+                () -> driver.getRawAxis(translationAxis),
+                () -> driver.getRawAxis(strafeAxis),
+                () -> speedReduction()));
+        operatorUP.onTrue(new TurnToAngleCmd(
+                s_Swerve,
+                0.0,
+                () -> driver.getRawAxis(translationAxis),
+                () -> driver.getRawAxis(strafeAxis),
+                () -> speedReduction()));
+        operatorDOWN.onTrue(new TurnToAngleCmd(
+                s_Swerve,
+                180.0,
+                () -> driver.getRawAxis(translationAxis),
+                () -> driver.getRawAxis(strafeAxis),
+                () -> speedReduction()));
+        operatorRIGHT.onTrue(new TurnToAngleCmd(
+                s_Swerve,
+                -90.0,
+                () -> driver.getRawAxis(translationAxis),
+                () -> driver.getRawAxis(strafeAxis),
+                () -> speedReduction()));
+        operatorLEFT.onTrue(new TurnToAngleCmd(
+                s_Swerve,
+                90.0,
+                () -> driver.getRawAxis(translationAxis),
+                () -> driver.getRawAxis(strafeAxis),
+                () -> speedReduction()));
     }
 
     /**
---
+++ src/main/java/frc/robot/commands/TurnToAngleCmd.java
@@ -0,0 +1,86 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+
+
+public class TurnToAngleCmd extends CommandBase {
+  Swerve swerveBase;
+  double angleDegrees;
+  DoubleSupplier m_forward;
+  DoubleSupplier m_strafe;
+  DoubleSupplier m_speedReduction;
+  private final SlewRateLimiter xLimiter, yLimiter;
+  final double ANGULAR_P = 0.1;
+  final double ANGULAR_D = 0.0;
+  PIDController turnController = new PIDController(ANGULAR_P, 0.0, ANGULAR_D);
+  double rotationSpeed;
+
+  /** Creates a new TurnToAngleCmd. */
+  public TurnToAngleCmd(Swerve swerveBase,
+  double angleDegrees,
+  DoubleSupplier forward,
+  DoubleSupplier strafe,
+  DoubleSupplier speedReduction) {
+  this.swerveBase = swerveBase;
+  this.angleDegrees = angleDegrees;
+  m_forward = forward;
+  m_strafe = strafe;
+  m_speedReduction = speedReduction;
+  turnController.enableContinuousInput(-180, 180);
+  this.xLimiter = new SlewRateLimiter(Constants.Swerve.kTeleDriveMaxAccelerationUnitsPerSecond);
+  this.yLimiter = new SlewRateLimiter(Constants.Swerve.kTeleDriveMaxAccelerationUnitsPerSecond);
+    addRequirements(swerveBase);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    SmartDashboard.putNumber("set point", angleDegrees);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    rotationSpeed = turnController.calculate(swerveBase.getYaw().getDegrees(), angleDegrees);
+
+    double fwdX = m_forward.getAsDouble();
+    double fwdY = m_strafe.getAsDouble();
+
+    // 2. Apply deadband
+    fwdX = Math.abs(fwdX) > 0.1 ? fwdX : 0.0;
+    fwdY = Math.abs(fwdY) > 0.1 ? fwdY : 0.0;
+
+
+    // 3. Make the driving smoother
+    fwdX = xLimiter.calculate(fwdX) * Constants.Swerve.kTeleDriveMaxSpeedMetersPerSecond * m_speedReduction.getAsDouble();
+    fwdY = yLimiter.calculate(fwdY) * Constants.Swerve.kTeleDriveMaxSpeedMetersPerSecond * m_speedReduction.getAsDouble();
+
+
+    swerveBase.drive(new Translation2d(-fwdX, -fwdY), rotationSpeed,true, true);
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
