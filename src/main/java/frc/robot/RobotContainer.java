78646bb8281ad9c9edd9fec6c8199d5ca1600d87
Laptop7 <Laptop7@milwaukeeroboticsacademy.org>
2023-02-27 16:17:16 -0600

    slowdownbutton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,6 +20,8 @@ import frc.robot.subsystems.*;
 public class RobotContainer {
     /* Controllers */
     private final Joystick driver = new Joystick(0);
+    private final Joystick operator =  new Joystick(1);
+  
 
     /* Drive Controls */
     private final int translationAxis = XboxController.Axis.kLeftY.value;
@@ -28,10 +30,11 @@ public class RobotContainer {
     private final int intakeAxis = XboxController.Axis.kLeftTrigger.value;
     private final int outtakeAxis = XboxController.Axis.kRightTrigger.value;
 
+
     /* Driver Buttons */
     private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
     private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
-    
+
 
 
     /* Subsystems */
