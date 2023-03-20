b69b255dc53a8acbf28bb634bd9afb97bb89b443
Laptop9 <laptop9@milwaukeeroboticsacademy.org>
2023-03-20 09:21:11 -0500

    autos

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -65,6 +65,13 @@ private double desiredHeading = 0;
         swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics, getYaw(), getModulePositions());
     }
 
+    /**
+     * Drift Correction driving
+     */
+    public void drive(ChassisSpeeds speeds){
+        SwerveModuleState[] states = getKinematics().toSwerveModuleStates(speeds);
+        setModuleStates(states);
+    }
     public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
        
        ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
